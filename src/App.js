8e5227eaa8c52c628ab3a525a4190de36e1a5c0e
JSH <qkdwn48@naver.com>
2022-04-06 23:59:16 +0900

    zustand MiddleWare
    1. persist
    액션을 처리하기 전에 추가적인 작업 진행

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import create from 'zustand'
-import {devtools} from "zustand/middleware";
+import {devtools, persist} from "zustand/middleware";
+import {useMemo} from "react";
 
-const useStore = create(devtools((set)=>({
+const useStore = create(persist((set)=>({
   count : 0,
-  up(){
+    counter : 0 ,
+  callCard(){
       set((state) => ({count : state.count + 1}))
-  }
+  },
+  callPerson(){
+      set((state) => ({counter : state.counter + 1}))
+  },
 })))
 function App() {
-  const {count, up} = useStore();
+    const {callCard, callPerson} = useStore();
   return (
     <div className="App">
-        <button onClick={() => {up();}}>버튼</button>
-      <p>사람 : {count}</p>
+        <button onClick={() => {callCard();}}>버튼</button>
+        <button onClick={() => {callPerson();}}>버튼</button>
+        <Person></Person>
       <Card></Card>
     </div>
   );
 }
 
 function Card() {
-    const {count} = useStore();
-  return (
+    const {counter} = useStore();
+    console.log('Card')
+  return useMemo (() => (
       <div className="App">
-        <p>카드 : {count}</p>
+        <p>카드 : {counter}</p>
       </div>
-  );
+  ), [counter]);
 }
 
+function Person() {
+    console.log('Person')
+    const {count} = useStore();
+    return useMemo ( () => (
+        <div className="App">
+            <p>사람 : {count}</p>
+        </div>
+    ), [count]);
+}
 
 
 export default App;
